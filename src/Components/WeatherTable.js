c33ac7b20c9c33a8b9c0121a740eeaadcdb7baea
kristocic <kristoenn@gmail.com>
2023-11-30 17:31:53 +0200

    fd

--- src/Components/WeatherTable.js
+++ src/Components/WeatherTable.js
@@ -1,33 +1,33 @@
-import React from 'react';
+const convertKmphToMps = (speedKmph) => {
+  const speedMps = speedKmph * 1000 / (60 * 60);
+  return speedMps.toFixed(1);
+}
 
-const WeatherTable = ({ dailyData }) => {
-  return (
-    <table>
+const convertDateStringToHoursAndMinutes = (dateString) => {
+  return `${(new Date(dateString)).getHours()}.${(new Date(dateString)).getMinutes()}`;
+}
+
+const WeatherTable = ({ weather }) => (
+  <table cellSpacing={24}>
       <thead>
-        <tr>
           <th>Date</th>
-          <th>Sunrise</th>
-          <th>Sunset</th>
-          <th>Temperature (°C)</th>
-          <th>Precipitation (mm)</th>
-          <th>Wind Speed (m/s)</th>
-          
-        </tr>
+          <th>Sunrise/sunset</th>
+          <th>Temp. min/max</th>
+          <th>Precipitation</th>
+          <th>Max wind</th>
       </thead>
       <tbody>
-        {dailyData.map((dailyData) => (
-          <tr key={dailyData.dailyData}>
-            <td>{dailyData.date}</td>
-            <td>{dailyData.sunrise}</td>
-            <td>{dailyData.sunset}</td>
-            <td>{dailyData.temperature}</td>
-            <td>{dailyData.precipitation}</td>
-            <td>{dailyData.windSpeed}</td>
-          </tr>
-        ))}
+          {weather.daily.time.map((_, i) => (
+              <tr>
+                  <td>{weather.daily.time[i]}</td>
+                  <td>{convertDateStringToHoursAndMinutes(weather.daily.sunrise[i])}/{convertDateStringToHoursAndMinutes(weather.daily.sunset[i])}</td>
+                  <td>{weather.daily.temperature_2m_min[i]}/{weather.daily.temperature_2m_max[i]}</td>
+                  <td>{weather.daily.precipitation_sum[i]}mm</td>
+                  <td>{convertKmphToMps(weather.daily.wind_speed_10m_max[i])}m/s</td>
+              </tr>
+          ))}
       </tbody>
-    </table>
-  );
-};
+  </table>
+);
 
 export default WeatherTable;
